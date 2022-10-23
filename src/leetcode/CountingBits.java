ab385b65f22fb51456ab894ca716fed0e3db74b3
choiyk <d_pvmf@naver.com>
2022-10-23 10:01:23 +0900

    리트코드 338. Counting Bits

---
+++ src/leetcode/CountingBits.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class CountingBits {
+
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        ans[0] = 0;
+
+        if(n == 0)
+            return ans;
+
+        ans[1] = 1;
+        int idx = 1; //계산할 기준 숫자 index
+        for(int i = 2; i <= n; i++) {
+            //홀수면 +1, 짝수면 +0
+            if(i % 2 == 0)
+                ans[i] = ans[idx];
+            else {
+                ans[i] = ans[idx] + 1;
+                idx++; //기준 index 변경
+            }
+        }
+
+        return ans;
+    }
+
+}
