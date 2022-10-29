434df494c21ebf2bbece604bf5f4d97d2c09583c
choiyk <d_pvmf@naver.com>
2022-10-29 10:07:03 +0900

    리트코드 77. Combinations

---
+++ test/leetcode/CombinationTest.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CombinationTest {
+
+    @Test
+    public void test() {
+        Combination o = new Combination();
+        int n = 4;
+        int k = 2;
+        o.combine(4, 2);
+    }
+
+}
