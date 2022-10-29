18734b2f01811163e6ba517c2ab089081adfa0ad
choiyk <d_pvmf@naver.com>
2022-10-29 09:28:49 +0900

    리트코드 784. Letter Case Permutation

---
+++ test/leetcode/LetterCasePermutationTest.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LetterCasePermutationTest {
+
+    @Test
+    public void test() {
+        String s = "a1b2";
+        LetterCasePermutation o = new LetterCasePermutation();
+        o.letterCasePermutation(s);
+    }
+
+}
