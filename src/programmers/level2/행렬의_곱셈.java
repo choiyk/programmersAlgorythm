44e439501db9f562d40fd7b9935d7c0b573ed652
choiyk <d_pvmf@naver.com>
2022-11-23 20:39:48 +0900

    프로그래머스 레벨2 행렬의 곱셈

---
+++ src/programmers/level2/행렬의_곱셈.java
@@ -0,0 +1,20 @@
+package programmers.level2;
+
+public class 행렬의_곱셈 {
+
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        //결과 배열의 사이즈는 int[arr1.length][arr2[0].length]
+
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for(int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr2[0].length; j++) {
+                for(int x = 0; x < arr1[0].length; x++) {
+                    answer[i][j] += arr1[i][x] * arr2[x][j];
+                }
+            }
+        }
+
+        return answer;
+    }
+}
